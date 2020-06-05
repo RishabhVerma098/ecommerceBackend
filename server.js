e6ec1ae2bab3cff39a60020f5c4bcce3f21c2b2f
rishabh verma <vermarishabh0987@gmail.com>
2020-06-05 14:43:29 +0530

    very basic setup done ✌

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const Express = require("express");
+const dotenv = require("dotenv");
+require("colors");
 
+//initialization
 const app = Express();
+dotenv.config({ path: "./config/config.env" });
 
+//main
 app.get("/", (req, res) => {
   res.send({
     sucesss: true,
     data: "hello world",
+    env: process.env.TEXT,
   });
 });
 
-app.listen(8000, console.log(`sever is running at Port:8000`));
+//port listen
+const port = process.env.PORT || 5000;
+
+app.listen(
+  port,
+  console.log(
+    `sever is running in ${process.env.NODE_ENV} at Port:${port}`.yellow.bold
+  )
+);
+
+//crash app on some unprocessed error
+process.on("unhandledRejection", (err, promisee) => {
+  console.log(`Error : ${err.message}`.red.bold);
+  server.close(() => process.exit(1));
+});
