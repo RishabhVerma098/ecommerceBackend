f790dc0e9f76bc78b0a5a7e63d20357fa2bf0e12
rishabh verma <vermarishabh0987@gmail.com>
2020-06-07 22:53:41 +0530

    added sentry

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const Express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
+const Sentry = require("@sentry/node");
 require("colors");
 
 //initialization
 const app = Express();
 dotenv.config({ path: "./config/config.env" });
 const connectDb = require("./config/db");
+Sentry.init({
+  dsn:
+    "https://ae78cfa4812c4f8dad30f606f9011e2f@o403974.ingest.sentry.io/5267425",
+});
 
 //import routes and middlewares
 const product = require("./routes/product");
@@ -14,12 +19,25 @@ const errorHandler = require("./middleware/errorHandler");
 const auth = require("./routes/user");
 
 //middleware
+// *The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
 app.use(morgan("dev"));
 app.use(Express.json());
 app.use("/api/v1/product", product);
 app.use("/api/v1/auth", auth);
+// * The error handler must be before any other error middleware and after all controllers
+app.use(
+  Sentry.Handlers.errorHandler({
+    serverName: false,
+    user: ["email"],
+  })
+);
 app.use(errorHandler);
 connectDb();
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
 //port listen
 const port = process.env.PORT || 5000;
